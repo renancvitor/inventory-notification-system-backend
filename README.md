22b243e1b75156b61bf7daff1b0785c2a4fd5dae
Renan C Vitor <renan.vitor.cm@gmail.com>
2025-09-04 16:08:57 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# inventory-notification-system-backend
---
+++ src/main/java/com/github/renancvitor/inventory/InventoryNotificationSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.renancvitor.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryNotificationSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryNotificationSystemBackendApplication.class, args);
+	}
+
+}
