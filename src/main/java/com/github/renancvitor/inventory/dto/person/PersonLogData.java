1c8457b440d6c1acd9ba811c87a1cc9beefda826
Renan C Vitor <renan.vitor.cm@gmail.com>
2025-09-30 22:03:33 -0300

    fix(person): change registrationDate type from LocalDateTime to String to resolve database compatibility issue

--- src/main/java/com/github/renancvitor/inventory/dto/person/PersonLogData.java
+++ src/main/java/com/github/renancvitor/inventory/dto/person/PersonLogData.java
@@ -1,22 +1,27 @@
 package com.github.renancvitor.inventory.dto.person;
 
-import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import com.github.renancvitor.inventory.domain.entity.person.Person;
 import com.github.renancvitor.inventory.infra.messaging.systemlog.LoggableData;
 
 public record PersonLogData(
-        Long id,
-        String personName,
-        String cpf,
-        String email,
-        LocalDateTime registrationDate) implements LoggableData {
-    public static PersonLogData fromEntity(Person person) {
-        return new PersonLogData(
-                person.getId(),
-                person.getPersonName(),
-                person.getCpf(),
-                person.getEmail(),
-                person.getRegistrationDate());
-    }
+                Long id,
+                String personName,
+                String cpf,
+                String email,
+                String registrationDate) implements LoggableData {
+
+        public static PersonLogData fromEntity(Person person) {
+                String formattedDate = person.getRegistrationDate() != null
+                                ? person.getRegistrationDate().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
+                                : null;
+
+                return new PersonLogData(
+                                person.getId(),
+                                person.getPersonName(),
+                                person.getCpf(),
+                                person.getEmail(),
+                                formattedDate);
+        }
 }
