205ad85d75d2dff8967b02696299beb9b037ebfe
Renan C Vitor <renan.vitor.cm@gmail.com>
2025-10-07 15:26:58 -0300

    refactor(email): update DTOs to handle list of recipients for better email privacy

--- src/main/java/com/github/renancvitor/inventory/dto/email/EmailLogData.java
+++ src/main/java/com/github/renancvitor/inventory/dto/email/EmailLogData.java
@@ -1,10 +1,12 @@
 package com.github.renancvitor.inventory.dto.email;
 
+import java.util.List;
+
 import com.github.renancvitor.inventory.infra.messaging.systemlog.LoggableData;
 
 public record EmailLogData(
         String subject,
-        String recipient,
+        List<String> recipient,
         String status) implements LoggableData {
 
     public static EmailLogData fromEmailRequest(EmailRequest emailRequest, String status) {
--- src/main/java/com/github/renancvitor/inventory/dto/email/EmailRequest.java
+++ src/main/java/com/github/renancvitor/inventory/dto/email/EmailRequest.java
@@ -1,10 +1,12 @@
 package com.github.renancvitor.inventory.dto.email;
 
+import java.util.List;
+
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 
 public record EmailRequest(
-        @NotNull @Email String recipient,
-        @NotNull String subject,
-        @NotNull String htmlBody) {
+                @NotNull @Email List<String> recipient,
+                @NotNull String subject,
+                @NotNull String htmlBody) {
 }
