001c3f56ee0ff3f1c6fc30f1473291dec58735af
Renan C Vitor <renan.vitor.cm@gmail.com>
2025-10-15 22:20:30 -0300

    refactor(user): adjust UserDetailData code indentation

--- src/main/java/com/github/renancvitor/inventory/application/user/dto/UserDetailData.java
+++ src/main/java/com/github/renancvitor/inventory/application/user/dto/UserDetailData.java
@@ -4,7 +4,10 @@ import com.github.renancvitor.inventory.domain.entity.user.User;
 import com.github.renancvitor.inventory.domain.entity.user.enums.UserTypeEnum;
 
 public record UserDetailData(
-        Long id, String personCpf, String personEmail, String nameUserType) {
+        Long id,
+        String personCpf,
+        String personEmail,
+        String nameUserType) {
 
     public UserDetailData(User user) {
         this(
