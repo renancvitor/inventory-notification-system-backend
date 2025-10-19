debf74f9328199237463c23068dae00cc58e1b52
Renan C Vitor <renan.vitor.cm@gmail.com>
2025-10-18 21:20:13 -0300

    fix(order): fix enum mapping in DTO constructor

--- src/main/java/com/github/renancvitor/inventory/application/order/dto/OrderDetailData.java
+++ src/main/java/com/github/renancvitor/inventory/application/order/dto/OrderDetailData.java
@@ -9,31 +9,29 @@ import com.github.renancvitor.inventory.domain.entity.order.Order;
 import com.github.renancvitor.inventory.domain.entity.order.enums.OrderStatusEnum;
 
 public record OrderDetailData(
-        Long id,
-        LocalDateTime createdAt,
-        String description,
-        BigDecimal totalValue,
-        String status,
-        String requestedBy,
-        String approvedBy,
-        String rejectedBy,
-        List<MovementDetailData> movements) {
-    public OrderDetailData(Order order) {
-        this(
-                order.getId(),
-                order.getCreatedAt(),
-                order.getDescription(),
-                order.getTotalValue(),
-                OrderStatusEnum.valueOf(order.getOrderStatus().getOrderStatusName()).getDisplayName() != null
-                        ? order.getRequestedBy().getUsername()
-                        : null,
-                order.getRequestedBy() != null ? order.getRequestedBy().getUsername() : null,
-                order.getApprovedBy() != null ? order.getApprovedBy().getUsername() : null,
-                order.getRejectedBy() != null ? order.getRejectedBy().getUsername() : null,
-                order.getMovements() != null
-                        ? order.getMovements().stream()
-                                .map(MovementDetailData::new)
-                                .toList()
-                        : List.of());
-    }
+                Long id,
+                LocalDateTime createdAt,
+                String description,
+                BigDecimal totalValue,
+                String status,
+                String requestedBy,
+                String approvedBy,
+                String rejectedBy,
+                List<MovementDetailData> movements) {
+        public OrderDetailData(Order order) {
+                this(
+                                order.getId(),
+                                order.getCreatedAt(),
+                                order.getDescription(),
+                                order.getTotalValue(),
+                                OrderStatusEnum.valueOf(order.getOrderStatus().getOrderStatusName()).getDisplayName(),
+                                order.getRequestedBy() != null ? order.getRequestedBy().getUsername() : null,
+                                order.getApprovedBy() != null ? order.getApprovedBy().getUsername() : null,
+                                order.getRejectedBy() != null ? order.getRejectedBy().getUsername() : null,
+                                order.getMovements() != null
+                                                ? order.getMovements().stream()
+                                                                .map(MovementDetailData::new)
+                                                                .toList()
+                                                : List.of());
+        }
 }
