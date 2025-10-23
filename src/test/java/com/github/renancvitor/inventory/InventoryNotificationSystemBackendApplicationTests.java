169021bbe2a6db45fa43025c1276801394797221
Renan C Vitor <renan.vitor.cm@gmail.com>
2025-10-23 11:39:47 -0300

    chore(test): add @MockBean for EmailService in test main class

--- src/test/java/com/github/renancvitor/inventory/InventoryNotificationSystemBackendApplicationTests.java
+++ src/test/java/com/github/renancvitor/inventory/InventoryNotificationSystemBackendApplicationTests.java
@@ -5,17 +5,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
+import com.github.renancvitor.inventory.application.email.service.EmailService;
 import com.github.renancvitor.inventory.infra.config.AwsSasConfig;
 
-@SpringBootTest(properties = {
-		"spring.config.location=classpath:/application-test.properties"
-})
+@SpringBootTest(classes = InventoryNotificationSystemBackendApplication.class)
 @ActiveProfiles("test")
 class InventoryNotificationSystemBackendApplicationTests {
 
 	@SuppressWarnings("removal")
 	@MockBean
 	private AwsSasConfig awsSasConfig;
+
+	@SuppressWarnings("removal")
+	@MockBean
+	private EmailService emailService;
 
 	@Test
 	void contextLoads() {
