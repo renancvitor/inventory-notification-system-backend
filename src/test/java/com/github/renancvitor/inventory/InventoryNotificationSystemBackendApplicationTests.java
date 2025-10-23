7f114f72cab0eefdbb6220407d133060e759d3d0
Renan C Vitor <renan.vitor.cm@gmail.com>
2025-10-23 11:35:15 -0300

    chore(test): add @MockBean for AwsSasConfig in test main class

--- src/test/java/com/github/renancvitor/inventory/InventoryNotificationSystemBackendApplicationTests.java
+++ src/test/java/com/github/renancvitor/inventory/InventoryNotificationSystemBackendApplicationTests.java
@@ -2,13 +2,20 @@ package com.github.renancvitor.inventory;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
+
+import com.github.renancvitor.inventory.infra.config.AwsSasConfig;
 
 @SpringBootTest(properties = {
 		"spring.config.location=classpath:/application-test.properties"
 })
 @ActiveProfiles("test")
 class InventoryNotificationSystemBackendApplicationTests {
+
+	@SuppressWarnings("removal")
+	@MockBean
+	private AwsSasConfig awsSasConfig;
 
 	@Test
 	void contextLoads() {
