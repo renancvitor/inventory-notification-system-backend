2b7cf70ef09aa71f942efaac98e2782851344784
Renan C Vitor <renan.vitor.cm@gmail.com>
2025-10-23 12:04:44 -0300

    chore(test): add @TestPropertySource to load application-test.properties in test main class

--- src/test/java/com/github/renancvitor/inventory/InventoryNotificationSystemBackendApplicationTests.java
+++ src/test/java/com/github/renancvitor/inventory/InventoryNotificationSystemBackendApplicationTests.java
@@ -4,13 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 import com.github.renancvitor.inventory.application.email.service.EmailService;
 import com.github.renancvitor.inventory.infra.config.AwsSasConfig;
 
-@SpringBootTest(classes = {
-		InventoryNotificationSystemBackendApplication.class }, webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest(classes = InventoryNotificationSystemBackendApplication.class, webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.properties")
 class InventoryNotificationSystemBackendApplicationTests {
 
 	@SuppressWarnings("removal")
