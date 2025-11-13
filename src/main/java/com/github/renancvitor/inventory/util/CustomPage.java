4cd60823972bf1352d69afd12ad5bc1a0f3798b5
Renan C Vitor <renan.vitor.cm@gmail.com>
2025-11-13 08:31:07 -0300

    refactor(utils): add getContent() method to CustomPage for easier access

--- src/main/java/com/github/renancvitor/inventory/util/CustomPage.java
+++ src/main/java/com/github/renancvitor/inventory/util/CustomPage.java
@@ -3,11 +3,15 @@ package com.github.renancvitor.inventory.util;
 import java.util.List;
 
 public record CustomPage<T>(
-        List<T> content,
-        int page,
-        int size,
-        long totalElements,
-        int totalPages,
-        boolean last) {
+                List<T> content,
+                int page,
+                int size,
+                long totalElements,
+                int totalPages,
+                boolean last) {
+
+        public List<T> getContent() {
+                return content;
+        }
 
 }
