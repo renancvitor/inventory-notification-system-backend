312dc31fb2ce72c3e7a5f1a11132962b88806a42
Renan C. Vitor <98927761+renancvitor@users.noreply.github.com>
2026-01-23 19:00:53 -0300

    refactor(kafka): implements RetryableTopic and DltHandler in Consumer

--- src/main/java/com/github/renancvitor/inventory/infra/messaging/kafka/consumer/OrderCreationKafkaConsumer.java
+++ src/main/java/com/github/renancvitor/inventory/infra/messaging/kafka/consumer/OrderCreationKafkaConsumer.java
@@ -1,27 +1,37 @@
 package com.github.renancvitor.inventory.infra.messaging.kafka.consumer;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Component;
 
 import com.github.renancvitor.inventory.infra.messaging.kafka.contract.KafkaTopics;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Component
 @ConditionalOnProperty(name = "messaging.provider", havingValue = "kafka")
 public class OrderCreationKafkaConsumer {
 
+    @RetryableTopic(attempts = "4", backoff = @Backoff(delay = 5000, multiplier = 2.0), retryTopicSuffix = "-RETRY", dltTopicSuffix = "-DLT")
     @KafkaListener(topics = KafkaTopics.ORDER_CREATED_V1, groupId = "order-created-consumer")
     public void consume(Object payload) {
-        System.out.println("Received Order Creation Event");
-        System.out.println(payload);
-
-        throw new RuntimeException("Erro proposital");
+        log.info("Received Order Creation Event: {}", payload);
     }
 
-    @KafkaListener(topics = KafkaTopics.ORDER_CREATED_DLT, groupId = "order-created-dlt-consumer")
-    public void dlq(Object payload) {
-        System.out.println("Received Order Creation DLT Event");
-        System.out.println(payload);
+    @DltHandler
+    public void dlq(ConsumerRecord<String, Object> record) {
+        log.error(
+                "DLT | topic={} | key={} | partition={} | offset={} | payload={}",
+                record.topic(),
+                record.key(),
+                record.partition(),
+                record.offset(),
+                record.value());
     }
 
 }
--- src/main/java/com/github/renancvitor/inventory/infra/messaging/kafka/consumer/StockBelowMinimumKafkaConsumer.java
+++ src/main/java/com/github/renancvitor/inventory/infra/messaging/kafka/consumer/StockBelowMinimumKafkaConsumer.java
@@ -1,25 +1,36 @@
 package com.github.renancvitor.inventory.infra.messaging.kafka.consumer;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Component;
 
 import com.github.renancvitor.inventory.infra.messaging.kafka.contract.KafkaTopics;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Component
 @ConditionalOnProperty(name = "messaging.provider", havingValue = "kafka")
 public class StockBelowMinimumKafkaConsumer {
 
+    @RetryableTopic(attempts = "4", backoff = @Backoff(delay = 5000, multiplier = 2.0), retryTopicSuffix = "-RETRY", dltTopicSuffix = "-DLT")
     @KafkaListener(topics = KafkaTopics.STOCK_BELOW_MINIMUM_V1, groupId = "stock-below-minimum-consumer")
     public void consume(Object payload) {
-        System.out.println("Received Stock Below Minimum Event");
-        System.out.println(payload);
+        log.info("Received Stock Below Minimum Event: {}", payload);
     }
 
-    @KafkaListener(topics = KafkaTopics.STOCK_BELOW_MINIMUM_DLT, groupId = "stock-below-minimum-dlt-consumer")
-    public void dlq(Object payload) {
-        System.out.println("Received Stock Below Minimum DLT Event");
-        System.out.println(payload);
+    @DltHandler
+    public void dlt(ConsumerRecord<String, Object> record) {
+        log.error(
+                "DLT | topic={} | key={} | partition={} | offset={} | payload={}",
+                record.topic(),
+                record.key(),
+                record.partition(),
+                record.offset(),
+                record.value());
     }
-
 }
