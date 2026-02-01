759259cede226fd8c8b1b72ece1d1c995240e21c
Renan C Vitor <renan.vitor.cm@gmail.com>
2026-02-01 18:01:16 -0300

    fix(security): fixes security filter to run health checker

--- src/main/java/com/github/renancvitor/inventory/infra/security/SecurityFilter.java
+++ src/main/java/com/github/renancvitor/inventory/infra/security/SecurityFilter.java
@@ -15,7 +15,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.validation.constraints.NotNull;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
