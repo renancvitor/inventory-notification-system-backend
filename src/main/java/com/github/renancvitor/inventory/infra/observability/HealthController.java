49c3f1f18822b6e8fb095a660e9bf1b7953b264a
Renan C Vitor <renan.vitor.cm@gmail.com>
2026-02-01 14:08:20 -0300

    chore(deploy): implements Health Checker

---
+++ src/main/java/com/github/renancvitor/inventory/infra/observability/HealthController.java
@@ -0,0 +1,39 @@
+package com.github.renancvitor.inventory.infra.observability;
+
+import java.time.OffsetDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @Value("${app.name}")
+    private String application;
+
+    @Value("${app.version}")
+    private String version;
+
+    @Value("${app.environment.default}")
+    private String environment;
+
+    @GetMapping
+    public ResponseEntity<Map<String, Object>> healthCheck() {
+        Map<String, Object> response = new HashMap<>();
+
+        response.put("status", "UP");
+        response.put("application", application);
+        response.put("version", version);
+        response.put("environment", environment);
+        response.put("timestamp", OffsetDateTime.now().toString());
+
+        return ResponseEntity.ok(response);
+    }
+
+}
